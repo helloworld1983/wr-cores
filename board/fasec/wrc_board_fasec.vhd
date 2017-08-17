7fe5dee4d0726008b22c7998e961842c1e068ce0
Grzegorz Daniluk <grzegorz.daniluk@cern.ch>
2017-08-17 13:46:15 +0200

    adding wrc_phy16.bram for wrpc-v4.1 release

--- board/fasec/wrc_board_fasec.vhd
+++ board/fasec/wrc_board_fasec.vhd
@@ -70,7 +70,7 @@ entity wrc_board_fasec is
     --g_tx_streamer_params       : t_tx_streamer_params := c_tx_streamer_params_defaut;
     --g_rx_streamer_params       : t_rx_streamer_params := c_rx_streamer_params_defaut;
     -- memory initialisation file for embedded CPU
-    g_dpram_initf               : string  := ""; --"/home/greg/wr/wr-cores/bin/wrpc/wrc_phy16.bram";
+    g_dpram_initf               : string  := "../../../../bin/wrpc/wrc_phy16.bram";
     -- identification (id and ver) of the layout of words in the generic diag interface
     g_diag_id                   : integer := 0;
     g_diag_ver                  : integer := 0;
